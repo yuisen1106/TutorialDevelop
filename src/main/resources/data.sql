82adab151c28858e0b7299ec42bd3b0b396792c7
yuisen1106 <yuvs2536@gmail.com>
2024-01-04 15:14:56 +0900

    Lesson 18Chapter 4.5 データベースの初期化

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user(name,gender,age,email)VALUES("キラメキ太郎","男性",27,'taro.kirameki@email.com');
+
+INSERT INTO user(name,gender,age,email)VALUES("キラメキ次郎","男性",22,'jiro.kirameki@email.com');
+
+INSERT INTO user(name,gender,age,email)VALUES("キラメキ花子","女性",25,'hanako.kirameki@email.com');

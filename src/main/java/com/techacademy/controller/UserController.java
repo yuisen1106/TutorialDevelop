9206c6d8ec6b898044268641648e74e0022242d6
yuisen1106 <yuvs2536@gmail.com>
2024-01-05 10:41:45 +0900

    Lesson 18Chapter 7.6 User登録機能を作る

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -3,8 +3,11 @@ package com.techacademy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
 
 @Controller
@@ -24,5 +27,19 @@ public class UserController {
 
 		return "user/list";
 
+	}
+
+	/**User登録画面を表示*/
+	@GetMapping("/register")
+	public String getRegister(@ModelAttribute User user) {
+
+		return "user/register";
+	}
+
+	/**User登録処理*/
+	@PostMapping("/register")
+	public String PostRegister(User user) {
+		service.saveUser(user);
+		return "redirect:/user/list";
 	}
 }
--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -3,6 +3,7 @@ package com.techacademy.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -19,8 +20,14 @@ public class UserService {
 	/**全件を検索して返す*/
 	public List<User> getUserList(){
 		return userRepository.findAll();
+
 	}
 
+	/**Userの登録を行う*/
+	@Transactional
+	public User saveUser(User user) {
+	return userRepository.save(user);
+	}
 
 
 }

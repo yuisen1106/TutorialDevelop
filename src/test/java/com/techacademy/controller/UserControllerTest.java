394c423f9791a344ea83c6bd455fcfb313b6b200
yuisen1106 <yuvs2536@gmail.com>
2024-01-07 18:21:37 +0900

    Lesson 18 Chapter 12 テスト駆動開発 Chapter 13 User情報の管理システムを完成させよう

---
+++ src/test/java/com/techacademy/controller/UserControllerTest.java
@@ -0,0 +1,93 @@
+package com.techacademy.controller;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.techacademy.entity.User;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+class UserControllerTest {
+
+	private MockMvc mockMvc;
+
+	private final WebApplicationContext webApplicationContext;
+
+
+	UserControllerTest(WebApplicationContext context){
+		this.webApplicationContext= context;
+	}
+
+	@BeforeEach
+    void beforeEach() {
+        // Spring Securityを有効にする
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply(springSecurity()).build();
+    }
+
+    @Test
+    @DisplayName("User更新画面")
+    @WithMockUser
+    void testGetUser() throws Exception {
+        // HTTPリクエストに対するレスポンスの検証
+        MvcResult result = mockMvc.perform(get("/user/update/1/")) // URLにアクセス
+            .andExpect(status().isOk()) // ステータスを確認
+            .andExpect(model().attributeExists("user")) // Modelの内容を確認
+		    .andExpect(model().hasNoErrors())
+		    .andExpect(view().name("user/update"))
+		    .andReturn();
+
+
+        User user=(User)result.getModelAndView().getModel().get("user");
+        assertEquals(user.getId(),1);
+        assertEquals(user.getName(),"キラメキ太郎");
+
+
+    }
+    @Test
+    @DisplayName("User一覧画面")
+    @WithMockUser
+    void testGetList() throws Exception{
+    	MvcResult result=mockMvc.perform(get("/user/list"))
+    			.andExpect(status().isOk())
+    			.andExpect(model().attributeExists("userlist"))
+    			.andExpect(model().hasNoErrors())
+    			.andExpect(view().name("user/list"))
+    			.andReturn();
+
+    	List<User> userlist=(List<User>)result.getModelAndView().getModel().get("userlist");
+    	assertEquals(userlist.size(),3);
+    	assertEquals(userlist.get(0).getId(),1);
+    	assertEquals(userlist.get(0).getName(),"キラメキ太郎");
+    	assertEquals(userlist.get(1).getId(),2);
+    	assertEquals(userlist.get(1).getName(),"キラメキ次郎");
+    	assertEquals(userlist.get(2).getId(),3);
+    	assertEquals(userlist.get(2).getName(),"キラメキ花子");
+
+
+
+
+    }
+}
